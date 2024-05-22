b832f3fa3723b3a9b356f3171f878176681ec66a
Philipp Dona <iqnitetechnologies@gmail.com>
2024-05-22 21:28:35 +0200

    Add more flexibility to allow use of more LEDs

--- ledrunner.py
+++ ledrunner.py
@@ -3,35 +3,35 @@ import board
 import neopixel
 from time import sleep
 
-gpio.setmode(gpio.BCM)
 
 # All the sequences that can be played
 GAMES = [
-    [0,0,0,0,0,0,1,2,3,3,2,1,3,2,3,1,3,1,3,2,3,2,1,2,2,1,3,3,3,1,0,0,0,0,0,0]
+    [1,2,3,3,2,1,3,2,3,1,3,1,3,2,3,2,1,2,2,1,3,3,3,1]
 ]
 
 
 # Setup the buttons
 PINRED = 23; PINGREEN = 24; PINBLUE = 25
+gpio.setmode(gpio.BCM)
 gpio.setup(PINRED, gpio.IN, gpio.PUD_UP)
 gpio.setup(PINGREEN, gpio.IN, gpio.PUD_UP)
 gpio.setup(PINBLUE, gpio.IN, gpio.PUD_UP)
 
+# Setup the LEDs
 PINPIXELS = board.D18
 AMOUNTPIXELS = 8
+USEDPIXELS = 8
 # Set brightness to a low number so no external supply is required
 pixels = neopixel.NeoPixel(PINPIXELS, AMOUNTPIXELS, brightness=0.01)
-
-# Only the first 6 LEDs are being used
 for i in range(8):
     pixels[i] = (0,0,0)
 
 
 def update(lst, active):
     global pixels
-    for i in range(6):
+    for i in range(USEDPIXELS):
         pixels[i] = (0,0,0)
-    leds = lst[active:(active+6)]
+    leds = lst[active:(active+USEDPIXELS)]
     for i in range(len(leds)):
         if   leds[i] == 0: pixels[i] = (0,0,0)
         elif leds[i] == 1: pixels[i] = (255,0,0)
@@ -73,18 +73,19 @@ while True:
             p = 2
         if p == 2 and noButtonPressed:
             p = 3
-        update([1,2,3,3,2,1,1,2,3,3,2,1], c)
-        if c >= 6: c = 0
+        # Show a scrolling idle sequence
+        update([1 + i%3 for i in range(3*USEDPIXELS)], c)
+        if c >= USEDPIXELS: c = 0
         else: c += 1
         sleep(0.1)
 
-    game = GAMES[0]
+    game = [0 for _ in range(USEDPIXELS)] + GAMES[0] + [0 for _ in range(USEDPIXELS)]
     delay = 0.5
     
     wrong = 0
     
     # Countdown
-    for i in range(6):
+    for i in range(USEDPIXELS):
         pixels[i] = (0,0,0)
     for i in range(3):
         pixels[i] = (255,0,0)
@@ -94,9 +95,11 @@ while True:
     print("Go!")
         
     # Game start
-    for led in range(len(game) - 6):
+    for led in range(len(game) - USEDPIXELS):
         update(game, led)
         sleep(delay)
         wrong += check(game[led])
     
-    print("Finished! Your accuracy: " + str(round((100 * (len(game)-12-wrong) / (len(game)-12)), 1)) + "%.")
+    print("Finished! Your accuracy: " + str(round(
+        (100 * (len(game)-(2*USEDPIXELS)-wrong) / (len(game)-(2*USEDPIXELS))), 
+        1)) + "%.")
